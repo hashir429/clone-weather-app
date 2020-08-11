45bdae0110e72d22a847e212f3e901aad20b46d1
Hahsir429 <65391970+hashir429@users.noreply.github.com>
2020-08-11 17:01:53 +0500

    change url

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-// const address = process.argv[2];
+const address = process.argv[2];
 
-// const url = `http://api.weatherstack.com/current?access_key=5d65501d5a614d46a203bdeded323cde&query=${address}`;
+const url = `http://api.weatherstack.com/current?access_key=5d65501d5a614d46a203bdeded323cde&query=${address}`;
 
 // request({url, json: true}, (error, response, body) => {
 //     if(error){
