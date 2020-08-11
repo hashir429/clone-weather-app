e6bb0d9f8bc02173e8680885d41f519226a59575
Hahsir429 <65391970+hashir429@users.noreply.github.com>
2020-08-11 16:16:37 +0500

    clone weather app

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const request = require("request");
+const nodemon = require("nodemon")
+
+const app = express();
+
+const address = process.argv[2];
+
+const url = `http://api.weatherstack.com/current?access_key=5d65501d5a614d46a203bdeded323cde&query=${address}`;
+
+request({url, json: true}, (error, response, body) => {
+    if(error){
+        console.log("can't connect to weather server");
+    }else{
+        console.log(`Name: ${body.request.query} || temperature: ${body.current.temperature} || Cloud: ${body.current.cloudcover}%`)
+    }
+});
+
+app.use(express.static(__dirname+"/public"));
+
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+})
---
+++ public/script.js
@@ -0,0 +1,25 @@
+//welcome to js
+//form submition
+const h1 = document.querySelector(".h1");
+const p = document.querySelector("p");
+const form = document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = document.querySelector("#search").value;
+    h1.textContent = `Loading...`;
+    p.textContent = "";
+    fetch(`http://api.weatherstack.com/current?access_key=5d65501d5a614d46a203bdeded323cde&query=${input}`).then(response => {
+        response.json().then(data => {
+            if (data.error) {
+                h1.textContent = "Enter Correct Location";
+            } else {
+                h1.textContent = `Name: ${data.request.query} || temperature: ${data.current.temperature} degree || Cloud: ${data.current.cloudcover}%`;
+                p.textContent = `${data.location.localtime}`;
+                if (data.current.is_day === "yes") {
+                    document.querySelector("body").style.cssText = 'background-image: url("../img/day.jpg"); background-size: cover';
+                } else {
+                    document.querySelector("body").style.cssText = 'background-image: url("../img/night.jpg"); color: white; background-size: cover'
+                }
+            }
+        })
+    })
+})
